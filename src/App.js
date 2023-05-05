201c2dbd473e506adb3921d52161e842cdc40dd0
Balasubramaniyan Nagabushanam <Balasubramaniyan.Nagabushanam@atkinsglobal.com>
2023-05-05 22:29:50 +0530

    leaflet update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-// import './leaflet/leaflet.css'
 import 'leaflet/dist/leaflet.css'
 
 function App() {
   const position = [13.095676, 77.578387]
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div>
-        <MapContainer className="map" center={position} zoom={20} scrollWheelZoom={false}>
+        <MapContainer className="map" center={position} zoom={18} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
+          {/* <Marker position={position}>
             <Popup>
               AARADHYA
             </Popup>
-          </Marker>
+          </Marker> */}
         </MapContainer>
       </div>
     </div>
